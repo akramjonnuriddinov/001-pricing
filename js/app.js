a36c24c5457d6725d7d42fe1102dde5f1d0da3c3
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2022-04-25 15:34:40 +0500

    Add js code and fix styles.css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-const elsPaymentPeriodRadio = querySelectorAll('[name="payment_period"]');
+const elsPaymentPeriodRadio = document.querySelectorAll('[name="payment_period"]');
+const elsPaymentPricingValue = document.querySelectorAll('.card-item-pricing-number');
 
 elsPaymentPeriodRadio.forEach(function (elPaymentPeriodRadio) {
     elPaymentPeriodRadio.addEventListener('change', function () {
-        console.log(elPaymentPeriodRadio.value);
+        const period = elPaymentPeriodRadio.value;
+        if (period === 'anual') {
+            elsPaymentPricingValue.forEach(function(elPrice) {
+                elPrice.textContent = elPrice.closest('.card-item').dataset.paymentAnual;
+            });
+        } else {
+            elsPaymentPricingValue.forEach(function(elPrice) {
+                elPrice.textContent = elPrice.closest('.card-item').dataset.paymentMonthly;
+            });
+        }
     });
 });
+  
